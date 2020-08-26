1781dee5d74fba46756a20c722425bc632241266
huuht <huahuu1@gmail.com>
2020-08-26 16:01:39 +0700

    first commit

--- ProjectHKIII-Website/dist/js/category-detail.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-$(document).ready(function () {
-  $('.button-collapse').click(function () {
-    $(this).toggleClass('rotate-up');
-  });
-});
--- ProjectHKIII-Website/dist/js/payment.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-$('.btnShow').click(function () {
-  if ($(this).parent().next().hasClass('d-none')) {
-    $(this).parent().next().removeClass('d-none');
-  } else {
-    $(this).parent().next().addClass('d-none');
-  }
-});
